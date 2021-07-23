ed6b17d3b611b793673483341c1a095ddda42815
dr-viridis <dr-viridis@users.noreply.github.com>
2021-07-22 20:35:10 -0500

    Create script1.py

---
+++ script1.py
@@ -0,0 +1,10 @@
+# A first python script
+# For the book Learning Python
+# Import a module, print some system information, print a number, play with strings
+
+import sys
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+print(x * 8)
+print('Hello world, wasn\'t I supposed to say that first?')
